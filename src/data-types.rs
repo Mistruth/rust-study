6286fefb710f26619952a66cbf5462771fd8ccf0
Mistruth <kikizxc4@qq.com>
2021-04-12 23:13:28 +0800

    common programming concept

---
+++ src/data-types.rs
@@ -0,0 +1,24 @@
+fn main() {
+  
+  // Scalar Types
+  
+  // int
+  let a: u32 = 16;
+  
+  // bool
+  let b: bool = true;
+
+  // char
+  let c = String::new();
+
+  // float
+  let d: f32 = 3.4;
+
+  // Compound Types
+  let e: (bool, u32, char) = (true, 32, '1');
+
+  // array
+  let f: [i32; 5] = [1,1,1,1,1]
+
+}
+
